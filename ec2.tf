fe080e78f5b9e365f63f16c67efec907e7828991
Charon <admin@cloudtinkerers.com>
2024-08-21 22:47:23 +0100

    changing code to t3.micro

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,16 @@
 data "aws_ami" "al2023_latest" {
   most_recent = true
-  name_regex = "^al2023-ami-[0-9]{4}.[0-9].[0-9]{8}.[0-9]-kernel-[0-9]+.[0-9]+-arm64$"
+  name_regex = "^al2023-ami-[0-9]{4}.[0-9].[0-9]{8}.[0-9]-kernel-[0-9]+.[0-9]+-x86_64"
   filter {
     name = "architecture"
-    values = ["arm64"]
+    values = ["x86_64"]
   }
   owners = ["amazon"]
 }
 
 resource "aws_instance" "web" {
   ami                         = data.aws_ami.al2023_latest.id
-  instance_type               = "t4g.micro"
+  instance_type               = "t3.micro"
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.main.id
   vpc_security_group_ids      = [ aws_security_group.main.id ]
